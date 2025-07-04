5a5490797eb21b0e17e26f9d21fe0a9bd5593620
RABIUL HASSAN <rabiulhassandev@gmail.com>
2025-07-04 12:46:10 +0800

    game functionality completed

--- src/Components/Home.vue
+++ src/Components/Home.vue
@@ -1,14 +1,21 @@
 <template>
-
         <MobileView>
-            <div class="text-center">
+            <div class="text-center" v-if="gameStore.isGameOver">
+                <h5 class="text-success" v-if="gameStore.score >= gameStore.winScore">Congratulations! You won the game!</h5>
+                <h5 class="text-danger" v-else>Game Over! You lost the game!</h5>
+                <h6 class="text-white pt-2">Your score: {{ gameStore.score }}</h6>
+                <h6 class="text-white pt-2">Attempt: {{ gameStore.attempt }}</h6>
+                <button class="btn btn-warning rounded-pill px-5 py-2 mt-2" @click="gameStore.resetGame()">Start Over</button>
+            </div>
+            <div class="text-center" v-else>
                 <h6 class="pt-2">Let's play the game</h6>
+                <p class="text-muted">
+                    <small><i>Max Attempt: {{ gameStore.maxAttempt }}, Win Score: {{ gameStore.winScore }}</i></small>
+                </p>
 
                 <h4 class="text-warning pt-3">Current Score: {{ gameStore.score }}</h4>
 
-                <h6 class="text-warning pt-5">Max Score: 0</h6>
-
-                <div class="pt-1">
+                <div class="pt-5">
                     <button class="btn btn-success rounded-pill px-4 py-2 mx-1" @click="incrementScore">Increament</button>
                     <button class="btn btn-danger rounded-pill px-4 py-2 mx-1" @click="decrementScore">Decreament</button>
                 </div>
@@ -25,7 +32,7 @@
     import MobileView from '@/Layouts/MobileView.vue'
     import { useGameStore  } from '@/store/gameStore'
 
-    const gameStore = useGameStore()
+    const gameStore = useGameStore()    
 
 
     // increment function
--- src/store/gameStore.js
+++ src/store/gameStore.js
@@ -6,11 +6,15 @@ export const useGameStore = defineStore("gameStore", {
         maxHealth: 100,
         maxAttack: 30,
         maxDefense: 10,
+        attempt: 0,
+        maxAttempt: 10,
+        winScore: 100,
+        isGameOver: false,
     }),
     getters: {
         // get score
-        getScore() {
-            return this.score;
+        getAttempt() {
+            return this.attempt;
         },
 
         health: (state) => state.maxHealth,
@@ -21,15 +25,37 @@ export const useGameStore = defineStore("gameStore", {
         // set next attact
         setNextAttack() {
             let attact = Math.floor(Math.random() * this.maxAttack) + 1;
-            console.log(`Attack: ${attact}`);            
+            console.log(`Attack: ${attact}`);
+            this.attempt++;
             this.score += attact;
+
+            // check if game is over
+            if (this.attempt >= this.maxAttempt || this.score >= this.winScore) {
+                this.isGameOver = true;
+                console.log("Game Over");
+            }
         },
 
         // set next defense
         setNextDefense() {
             let defense = Math.floor(Math.random() * this.maxDefense) + 1;
             console.log(`Defense: ${defense}`);
+            this.attempt++;
             this.score -= defense;
+
+            // check if game is over
+            if (this.attempt >= this.maxAttempt || this.score >= 100) {
+                this.isGameOver = true;
+                console.log("Game Over");
+            }
+        },
+
+        // reset game
+        resetGame() {
+            this.score = 0;
+            this.attempt = 0;
+            this.isGameOver = false;
+            console.log("Game Reset");
         },
     },
 })
