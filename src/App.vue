53583c1381f526f71734f4cde269949f0818589b
RABIUL HASSAN <rabiulhassandev@gmail.com>
2025-07-04 10:24:52 +0800

    Pinia configured

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,16 @@
 <template>
-    <h1>Welcome to My Vue App</h1>
+
+  <header class="pt-2">
+    <router-link aria-current="page" :to="{name: 'home'}" class="btn btn-outline-warning rounded-pill px-5 py-2 mx-1">Home</router-link>
+    <router-link aria-current="page" :to="{name: 'about'}" class="btn btn-outline-warning rounded-pill px-5 py-2 mx-1">About</router-link>
+    <router-link aria-current="page" :to="{name: 'info'}" class="btn btn-outline-warning rounded-pill px-5 py-2 mx-1">Info</router-link>
+  </header>
+
+  
+  <router-view></router-view>
 </template>
+
+
+<script setup>
+
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import App from '@/App.vue'
+import Home from '@/Components/Home.vue'
+import Info from '@/Components/Info.vue'
+import About from '@/Components/About.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -7,9 +9,20 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: App,
+      component: Home
+    },
+    {
+      path: '/about',
+      name: 'about',
+      component: About
+    },
+    {
+      path: '/info',
+      name: 'info',
+      component: Info
     }
   ],
+  linkActiveClass: "active",
 })
 
 export default router
