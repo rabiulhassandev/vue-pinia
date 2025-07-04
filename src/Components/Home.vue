0a790aada92fc4d6efcbf7f14150ba003c057651
RABIUL HASSAN <rabiulhassandev@gmail.com>
2025-07-04 12:09:06 +0800

    call the basic functions of the game

--- src/Components/Home.vue
+++ src/Components/Home.vue
@@ -4,16 +4,16 @@
             <div class="text-center">
                 <h6 class="pt-2">Let's play the game</h6>
 
-                <h4 class="text-warning pt-3">Current Score: 0</h4>
+                <h4 class="text-warning pt-3">Current Score: {{ gameStore.score }}</h4>
 
                 <h6 class="text-warning pt-5">Max Score: 0</h6>
 
                 <div class="pt-1">
-                    <button class="btn btn-success rounded-pill px-4 py-2 mx-1">Increament</button>
-                    <button class="btn btn-danger rounded-pill px-4 py-2 mx-1">Decreament</button>
+                    <button class="btn btn-success rounded-pill px-4 py-2 mx-1" @click="incrementScore">Increament</button>
+                    <button class="btn btn-danger rounded-pill px-4 py-2 mx-1" @click="decrementScore">Decreament</button>
                 </div>
 
-                <button class="btn btn-primary rounded-pill px-5 py-2 mt-2">Random</button>
+                <button class="btn btn-primary rounded-pill px-5 py-2 mt-2" @click="randomScore">Random</button>
             </div>
             
         </MobileView>
@@ -22,6 +22,25 @@
 
 
 <script setup lang="ts">
-    import MobileView from '@/Layouts/MobileView.vue';
+    import MobileView from '@/Layouts/MobileView.vue'
+    import { useGameStore  } from '@/store/gameStore'
+
+    const gameStore = useGameStore()
+
+
+    // increment function
+    function incrementScore() {
+        gameStore.setNextAttack()
+    }
+
+    // decrement function
+    function decrementScore() {
+        gameStore.setNextDefense()
+    }
+
+    // random function
+    function randomScore() {
+        Math.random() < 0.5 ? incrementScore() : decrementScore()
+    }
 
 </script>
--- src/store/gameStore.js
+++ src/store/gameStore.js
@@ -6,5 +6,30 @@ export const useGameStore = defineStore("gameStore", {
         maxHealth: 100,
         maxAttack: 30,
         maxDefense: 10,
-    })
+    }),
+    getters: {
+        // get score
+        getScore() {
+            return this.score;
+        },
+
+        health: (state) => state.maxHealth,
+        attack: (state) => state.maxAttack,
+        defense: (state) => state.maxDefense,
+    },
+    actions: {
+        // set next attact
+        setNextAttack() {
+            let attact = Math.floor(Math.random() * this.maxAttack) + 1;
+            console.log(`Attack: ${attact}`);            
+            this.score += attact;
+        },
+
+        // set next defense
+        setNextDefense() {
+            let defense = Math.floor(Math.random() * this.maxDefense) + 1;
+            console.log(`Defense: ${defense}`);
+            this.score -= defense;
+        },
+    },
 })
